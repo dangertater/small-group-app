1d1a5fe7d257775adf3fe0f2087272e9e4407805
dangertater <106702944+dangertater@users.noreply.github.com>
2023-01-31 11:05:03 -0700

    todoList.txt implemented for organization

--- my-app/src/components/inputs.tsx
+++ my-app/src/components/inputs.tsx
@@ -1,6 +1,9 @@
 import React from "react"
-import Input from "@mui/material/Input"
+import { Input } from "@mui/material"
 
 export function GeneralInputWindow(props: { placeholder: string }) {
-	return <Input></Input>
+	//TODO:idk why the below line doesn't work, commented out and have placeholder
+	//----input while it is figured out
+	// return <Input placeholder=`${props.placeholder}`></Input>
+	return <Input placeholder="heck"></Input>
 }
