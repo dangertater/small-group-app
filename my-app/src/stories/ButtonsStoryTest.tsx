4dd84a2f2e0b5800196c21b6ec6aa887737843ca
dangertater <106702944+dangertater@users.noreply.github.com>
2023-02-02 07:09:42 -0700

    added test button in story folder

---
+++ my-app/src/stories/ButtonsStoryTest.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Button } from "@mui/material"
+// import { ReactComponent as GreenMenuButton } from "../images/greenMenuButton.svg"
+//TODO: import green lines as the menu button
+//general use 'Submit' button
+export function SubmitButton(props: { children?: string }) {
+	return (
+		<Button variant="contained" color="secondary" size="large">
+			Submit
+		</Button>
+	)
+}
+//three lined menu button
+export function NavBarButton(props: { children?: string }) {
+	return (
+		<Button variant="contained" size="small">
+			{/* <GreenMenuButton></GreenMenuButton> */}
+			<img src="../src/images/greenMenuButton.svg" alt="heck"></img>
+		</Button>
+	)
+}
